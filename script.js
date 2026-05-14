af39ea28937ec9ffde70c589bed758e04c686d5e
F1roz <feroz.fahim.9@gmail.com>
2026-05-14 12:58:11 +0600

    “Updated”

--- script.js
+++ script.js
@@ -1074,14 +1074,18 @@ function buildMailto(ep, member) {
   const body    = encodeURIComponent([
     // `Hi ${member?.name || 'Team'},`, '',
     `Hi ${ep.name},`,
+
     'Exchange Participant Details:', '',
     `Name:          ${ep.name}`,
     `Email:         ${ep.email}`,
     `Phone:         ${ep.phone || 'N/A'}`,
     `University:    ${ep.university || 'N/A'}`,
+
     `Your login credentials for the AIESEC platform are as follows:`,
+
     `Website: https://www.aiesec.org`,
     `Password: 1234@Aiesec`,
+    
     `Start your global experience today—reach out to AIESEC in Bangladesh to explore available exchange opportunities`,
     // `Department:    ${ep.department || 'N/A'}`,
     // `Focus Product: ${ep.focusProduct || 'N/A'}`,
